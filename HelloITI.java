229066abeb1a1893294b36d465ee78eccdb045a6
Hanaa Hany <hanaahany822@gmail.com>
2023-09-03 12:53:13 +0300

    add ignoring file

--- HelloITI.java
+++ HelloITI.java
@@ -2,9 +2,13 @@ public class HelloITI{
 	public static void helloITI(){
 		System.out.println("Hello ITI");
 	}
+	public static void helloGit(){
+		System.out.println("Hello GIT");
+	}
 	public static void main(String []args){
 		
 		helloITI();
+		helloGit();
 	}
 
 }
