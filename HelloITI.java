e423a414b7e32324708cde2a18a37410241e5b03
mohannadahmed00 <mohannad199344@gmail.com>
2023-09-04 13:41:58 +0200

    print java method has been added

--- HelloITI.java
+++ HelloITI.java
@@ -5,10 +5,14 @@ public class HelloITI{
 	public static void helloGit(){
 		System.out.println("Hello GIT");
 	}
+	public static void helloJava(){
+		System.out.println("Hello Java");
+	}
 	public static void main(String []args){
 		
 		helloITI();
 		helloGit();
+		helloJava();
 	}
 
 }
